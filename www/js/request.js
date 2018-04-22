68600bc53966e531928edab836e61eb861fee6ca
goparklubanic <ngoparios@yahoo.com>
2018-04-22 08:27:24 +0700

    eval 3 geolocation

--- www/js/request.js
+++ www/js/request.js
@@ -1,3 +1,51 @@
+$(document).ready(function() {
+    // are we running in native app or in a browser?
+    window.isphone = false;
+    if(document.URL.indexOf("http://") === -1
+        && document.URL.indexOf("https://") === -1) {
+        window.isphone = true;
+    }
+    console.log(window.isphone);
+    if( window.isphone ) {
+        document.addEventListener("deviceready", this.onDeviceReady, false);
+        console.log('native app');
+        getCoord();
+    } else {
+        onDeviceReady();
+        console.log('broser');
+    }
+});
+
+function onDeviceReady() {
+    // do everything here.
+    console.log('onDeviceReady');
+    getCoord();
+}
+
+function getCoord(){
+  console.log('getCoord');
+  var geopsi = {
+    enableHighAccuracy: true,
+    timeout : 100000,
+    maximumAge: 3600000
+  }
+
+  navigator.geolocation.getCurrentPosition(geoSuccess,geoWurung,geopsi);
+}
+
+var geoSuccess = function(posisi){
+  var titikJemput = posisi.coords.latitude+","+posisi.coords.longitude;
+  documnent.getElementById('geol').value = titikJemput;
+}
+
+var geoWurung = function(rusak){
+  var ecode = rusak.code;
+  var mksud = rusak.message;
+
+  document.getElementById('ecode').value=ecode;
+  document.getElementById('edesc').value=mksud;
+}
+/*
 var ambClient = {
     // Application Constructor
     initialize: function() {
@@ -17,26 +65,7 @@ var ambClient = {
     }
 
     getCoord: function() {
-      var geopsi = {
-        enableHighAccuracy: true,
-        timeout : 100000,
-        maximumAge: 3600000
-      }
 
-      var geoSuccess = function(posisi){
-        var titikJemput = posisi.coords.latitude+","+posisi.coords.longitude;
-        documnent.getElementById('geol').value = titikJemput;
-      }
-
-      var geoWurung = function(rusak){
-        var ecode = rusak.code;
-        var mksud = rusak.message;
-
-        document.getElementById('ecode').value=ecode;
-        document.getElementById('edesc').value=mksud;
-      }
-
-      navigator.geolocation.getCurrentPosition(geoSuccess,geoWurung,geopsi);
 
     },
 
@@ -49,3 +78,5 @@ var ambClient = {
       }
     }
 };
+
+*/
