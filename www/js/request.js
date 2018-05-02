95da32de2bd7fff49c8fd54563ecd554737e58a8
goparklubanic <ngoparios@yahoo.com>
2018-05-02 07:00:34 +0700

    chg server address

--- www/js/request.js
+++ www/js/request.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
     localStorage.setItem('ablScore',0);
 
-    $.post('http://ngh.mugeno.org/sibulan/resi/users.php',{
+    $.post('https://klubaners.web.id/sibulan/resi/users.php',{
       op : 'ticket'
     },function(tiket){
       $('#ticketId').val(tiket);
